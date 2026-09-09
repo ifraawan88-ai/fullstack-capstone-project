dafa6a3f6e18c2e93ebe2e17a3b8f941bf8bae8e
ifra <ifraawan.88@gmail.com>
2026-09-10 00:19:30 +0500

    Create db.js

---
+++ db.js
@@ -0,0 +1,10 @@
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.MONGODB_URI);
+
+async function connectToDatabase() {
+  await client.connect();
+  return client.db("giftlink");
+}
+
+module.exports = { connectToDatabase };
